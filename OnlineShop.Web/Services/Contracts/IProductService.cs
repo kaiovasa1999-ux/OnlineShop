9a744ce64dd28af32bcfde0e5404d8e6f82e20a5
Veselin Valkanov <veselin.d.valkanov@gmail.com>
2022-08-09 20:54:18 +0300

    Added mew razer File and new Service

---
+++ OnlineShop.Web/Services/Contracts/IProductService.cs
@@ -0,0 +1,11 @@
+﻿using ShopOnline.Model.DTO;
+
+namespace OnlineShop.Web.Services.Contracts
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductDTO>> GetItems();
+
+        Task<ProductDTO> GetProduct(int id);
+    }
+}
---
+++ OnlineShop.Web/Services/ProductService.cs
@@ -0,0 +1,34 @@
+﻿using OnlineShop.Web.Services.Contracts;
+using ShopOnline.Model.DTO;
+using System.Net.Http.Json;
+
+namespace OnlineShop.Web.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly HttpClient httpClient;
+        public ProductService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public async Task<IEnumerable<ProductDTO>> GetItems()
+        {
+            try
+            {
+                var product = await this.httpClient.GetFromJsonAsync<IEnumerable<ProductDTO>>("api/Product");
+                return product;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public Task<ProductDTO> GetProduct(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
