ff2947e383af87f06486ecee55326de83c420be6
Veselin Valkanov <veselin.d.valkanov@gmail.com>
2022-08-09 19:14:52 +0300

    Fix the project type

--- OnlineShop.Web/Program.cs
+++ OnlineShop.Web/Program.cs
@@ -1,14 +1,11 @@
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using ShopOnline.Web.Data;
+using OnlineShop.Web;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-// Add services to the container.
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7012/")  });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-var app = builder.Build();
-
+await builder.Build().RunAsync();
